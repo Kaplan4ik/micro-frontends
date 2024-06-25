fc8bc4885878420816e65b55dc8813b1df0b614a
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-06-25 08:31:34 +0300

    Check workflow

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -224,7 +224,6 @@ export default function Pricing() {
           ))}
         </Grid>
       </Container>
-      {/* Footer */}
       <Container maxWidth="md" component="footer" className={classes.footer}>
         <Grid container spacing={4} justifyContent="space-evenly">
           {footers.map((footer) => (
