498192361e6571130b70c15645ad2a574bb88970
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-07-06 13:08:26 +0300

    Create auth module

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -1,26 +1,24 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
-
-const domain = process.env.PRODUCTION_DOMAIN;
+const { dependencies } = require('../package.json');
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/',
+    publicPath: '/auth/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap',
       },
-      shared: packageJson.dependencies,
+      shared: dependencies,
     }),
   ],
-};
+}
 
 module.exports = merge(commonConfig, prodConfig);
